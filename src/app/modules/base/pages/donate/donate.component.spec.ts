999e6c4a134ccae02176d9969ff5373fdf6244df
Carter Maxwell <cmaxwell@imsa.edu>
2021-02-26 14:14:11 -0600

    Basic Donation Functionality

---
+++ src/app/modules/base/pages/donate/donate.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { DonateComponent } from './donate.component';
+
+describe('DonateComponent', () => {
+  let component: DonateComponent;
+  let fixture: ComponentFixture<DonateComponent>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [DonateComponent],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DonateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/base/pages/donate/donate.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donate',
+  templateUrl: './donate.component.html',
+  styleUrls: ['./donate.component.css'],
+})
+export class DonateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
